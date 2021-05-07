e861ba44bbeab864d1fabc0b6000a185cd697312
Pli <4444175+Plixs@users.noreply.github.com>
2021-05-07 18:56:14 +0800

    Update bitwarden_rs_heroku.sh
    fix illegal option -- p error

--- bitwarden_rs_heroku.sh
+++ bitwarden_rs_heroku.sh
@@ -141,7 +141,7 @@ function help {
     printf "Welcome to help!\Use option -a for app name,\n-d <0/1> to enable duo,\n -g to set a git hash to clone bitwarden_rs from,\n and -t to specify if deployment or update!"
 }
 
-while getopts a:b:d:g:t:u:v: flag
+while getopts a:b:d:g:p:s:t:u:v: flag
 do
     case "${flag}" in
         a) CREATE_APP_NAME=${OPTARG};;
